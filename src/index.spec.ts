80cd5c306c1ab3ad4e5631da05cd45e01d28ae39
Emile Fugulin <code@efugulin.com>
2024-06-26 15:39:49 -0400

    Add initial validator

---
+++ src/index.spec.ts
@@ -0,0 +1,28 @@
+import fs from "fs";
+
+import { describe, expect, it } from "vitest";
+
+import { validateManifest } from "./validator.js";
+
+describe("Manifest", () => {
+  it("Validate frontend manifest", () => {
+    const data = JSON.parse(
+      fs.readFileSync("./tests/valid_frontend.json", "utf-8"),
+    );
+    expect(validateManifest(data)).toBe(true);
+  });
+
+  it("Validate fullstack manifest", () => {
+    const data = JSON.parse(
+      fs.readFileSync("./tests/valid_fullstack.json", "utf-8"),
+    );
+    expect(validateManifest(data)).toBe(true);
+  });
+
+  it("Validate invalid version manifest", () => {
+    const data = JSON.parse(
+      fs.readFileSync("./tests/invalid_version.json", "utf-8"),
+    );
+    expect(validateManifest(data)).toBe(false);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { validateManifest } from "./validator.js";
